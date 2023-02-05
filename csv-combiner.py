7927a7d4c7e3e07b00acc89c6dce003ed90ca666
Justin Quach-Law <quachlaj@oregonstate.edu>
2023-02-05 17:51:51 -0600

    Separated code into smaller functions and added error handling

--- csv-combiner.py
+++ csv-combiner.py
@@ -3,6 +3,7 @@ import sys
 
 import pandas as pd
 
+
 """
 Convert multiple .csv to dataframe
 Print to terminal or redirect with '>'
@@ -12,17 +13,43 @@ $ python csv-combiner.py ./fixtures/accessories.csv ./fixtures/clothing.csv
 $ python csv-combiner.py ./fixtures/accessories.csv ./fixtures/clothing.csv > combined.csv
 """
 
-# empty dataframe
-list_of_frames = []
-for arg in sys.argv[1:]:
-    # dataframe
-    df = pd.read_csv(arg)
-    # basename column
-    df["filename"] = os.path.basename(arg)
-    # add dataframe to list
-    list_of_frames.append(df)
 
-# concat all dataframes
-result = pd.concat(list_of_frames, sort=False)
-# convert to csv
-result.to_csv(sys.stdout, index=False)
+def check_file_type():
+    if len(sys.argv) == 0:
+        pass
+
+    for arg in sys.argv[1:]:
+        if not arg.endswith(".csv"):
+            print(f"Error: Expected .csv file", file=sys.stderr)
+            sys.exit(1)
+
+
+def combine_csv():
+    # empty list to hold dataframe(s)
+    list_of_frames = []
+
+    for arg in sys.argv[1:]:
+        # csv to dataframe
+        try:
+            df = pd.read_csv(arg)
+        except FileNotFoundError:
+            print(f"Error: File not found: '{arg}'", file=sys.stderr)
+            sys.exit(1)
+        except pd.errors.ParserError as e:
+            print(f"Error: Parsing error: '{arg}'\n", e, file=sys.stderr)
+            sys.exit(1)
+
+        # add basename column
+        df["filename"] = os.path.basename(arg)
+        # add dataframe to list
+        list_of_frames.append(df)
+
+    # concatenate all dataframes
+    result = pd.concat(list_of_frames, sort=False)
+    # convert to csv & output
+    result.to_csv(sys.stdout, index=False)
+
+
+if __name__ == "__main__":
+    check_file_type()
+    combine_csv()
