bc90bb4b055329e09d04d17b14bcd2ba083cd204
Justin Quach-Law <quachlaj@oregonstate.edu>
2023-02-05 17:16:07 -0600

    Convert a .csv to dataframe and print to terminal

---
+++ csv-combiner.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import pandas as pd
+
+"""
+Convert .csv to dataframe
+Print to terminal
+
+Terminal execution:
+$ python csv-combiner.py ./fixtures/accessories.csv
+"""
+
+# dataframe
+df = pd.read_csv(sys.argv[1])
+
+# basename column
+df['filename'] = os.path.basename(sys.argv[1])
+
+# convert to csv
+df = df.to_csv(index=False)
+
+# print(df)
+sys.stdout.write(df)
