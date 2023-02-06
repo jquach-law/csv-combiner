c151c01b2713054f5c08ca15b97eb3b051a71b0e
Justin Quach-Law <quachlaj@oregonstate.edu>
2023-02-05 18:14:15 -0600

    Updated error handling and more concise functions

--- csv-combiner.py
+++ csv-combiner.py
@@ -3,52 +3,56 @@ import sys
 
 import pandas as pd
 
-"""
-Convert multiple .csv to dataframe
-Print to terminal or redirect with '>'
 
-Terminal execution:
-$ python csv-combiner.py ./fixtures/accessories.csv ./fixtures/clothing.csv
-$ python csv-combiner.py ./fixtures/accessories.csv ./fixtures/clothing.csv > combined.csv
-"""
+def validate_input_files():
+    """
+    DOCSTRING
+    """
+    # Validate at least one argument is given
+    if len(sys.argv) <= 1:
+        raise ValueError(f"Error: Need at least 1 .csv file, 0 is given")
 
-
-def check_file_type():
-    if len(sys.argv) == 0:
-        pass
-
+    # Validate files end with .csv
     for arg in sys.argv[1:]:
         if not arg.endswith(".csv"):
-            print("Error: Expected .csv file", file=sys.stderr)
-            sys.exit(1)
+            raise ValueError(f"Error: Expected .csv file, received {arg}")
+
+
+def read_csv(file_path):
+    """
+    DOCSTRING
+    """
+    # Read .csv to dataframe
+    try:
+        df = pd.read_csv(file_path)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Error: File not found: '{file_path}'")
+    except pd.errors.ParserError as e:
+        raise pd.errors.ParserError(f"Error: Parsing error: '{file_path}'")
+
+    # Add 'filename' column to dataframe
+    df["filename"] = os.path.basename(file_path)
+
+    return df
 
 
 def combine_csv():
-    # empty list to hold dataframe(s)
-    list_of_frames = []
-
-    for arg in sys.argv[1:]:
-        # csv to dataframe
-        try:
-            df = pd.read_csv(arg)
-        except FileNotFoundError:
-            print(f"Error: File not found: '{arg}'", file=sys.stderr)
-            sys.exit(1)
-        except pd.errors.ParserError as e:
-            print(f"Error: Parsing error: '{arg}'\n", e, file=sys.stderr)
-            sys.exit(1)
-
-        # add basename column
-        df["filename"] = os.path.basename(arg)
-        # add dataframe to list
-        list_of_frames.append(df)
-
-    # concatenate all dataframes
+    """
+    DOCSTRING
+    """
+    # Create and hold dataframe for each .csv argument
+    list_of_frames = [read_csv(arg) for arg in sys.argv[1:]]
+    # Concatenate all dataframes
     result = pd.concat(list_of_frames, sort=False)
-    # convert to csv & output
+    # Convert to csv & output
     result.to_csv(sys.stdout, index=False)
 
 
 if __name__ == "__main__":
-    check_file_type()
-    combine_csv()
+    # Try-Block to catch raised exceptions
+    try:
+        validate_input_files()
+        combine_csv()
+    except (FileNotFoundError, pd.errors.ParserError, ValueError) as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
